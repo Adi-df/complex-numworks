212085fea08c61504af2faa3dfab7c1cc7ce78af
Adi-df <adidf-web@laposte.net>
2022-10-31 16:13:48 +0100

    Forgot icon

--- build.rs
+++ build.rs
@@ -3,8 +3,15 @@ use std::process::Command;
 fn main() {
     // Turn icon.png into icon.nwi
     println!("cargo:rerun-if-changed=src/icon.png");
-    let output = Command::new("nwlink")
-        .args(&["png-nwi", "src/icon.png", "target/icon.nwi"])
+    let output = Command::new("npm")
+        .args(&[
+            "exec",
+            "--yes",
+            "nwlink",
+            "png-nwi",
+            "src/icon.png",
+            "target/icon.nwi",
+        ])
         .output()
         .expect("Failure to launch process");
     assert!(
